a4339a65b7cdd4b6384291ef5a39bd18d478e27c
Felipe Cleverson <felipecleverson1@gmail.com>
2026-05-02 12:44:00 -0400

    atlas_books

---
+++ src/components/pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from "../../img/livro.png";
+
+export const livros = [
+  { nome: "Liderança em Design", id: 1, src: livro },
+  { nome: "Javascript Assertivo", id: 2, src: livro },
+  { nome: "Cangaceiro em javaScript", id: 3, src: livro },
+  { nome: "Aventureiros do java", id: 4, src: livro },
+];
--- src/components/pesquisa/index.js
+++ src/components/pesquisa/index.js
@@ -1,5 +1,7 @@
 import Input from "../input";
 import styled from "styled-components";
+import { useState } from "react";
+import { livros } from "./dadosPesquisa";
 
 const PesquisaContainer = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -23,11 +25,30 @@ const SubTitulo = styled.h3`
 `;
 
 function Pesquisa() {
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+
+  console.log(livrosPesquisados);
+
   return (
     <PesquisaContainer>
       <Titulo>já sabe por onde começar?</Titulo>
       <SubTitulo>Encontre seu livro em nossa estante</SubTitulo>
-      <Input placeholder="Escreva sua próximo leitura" />
+      <Input
+        placeholder="Escreva sua próximo leitura"
+        onBlur={(evento) => {
+          const textoDigitado = evento.target.value;
+          const resutaldoPesquisa = livros.filter(
+            (livro) => livro.nome.includes(textoDigitado),
+            setLivrosPesquisados(resutaldoPesquisa),
+          );
+        }}
+      />
+      {livrosPesquisados.map((livro) => (
+        <div>
+          <p>{livro.name}</p>
+          <img src={livro.src} />
+        </div>
+      ))}
     </PesquisaContainer>
   );
 }
